f8fa0e31d073395e4d16531def602915e103a092
May Thazin Hlaing <99190813+maythazinh@users.noreply.github.com>
2025-03-04 18:23:06 +0900

    duble final

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,7 +1,7 @@
-resource "aws_s3_bucket" "website" {
-  bucket = var.bucket_name
-  tags   = var.tags
-}
+# resource "aws_s3_bucket" "website" {
+#   bucket = var.bucket_name
+#   tags   = var.tags
+# }
 
 resource "aws_s3_bucket_website_configuration" "website" {
   bucket = aws_s3_bucket.website.id
