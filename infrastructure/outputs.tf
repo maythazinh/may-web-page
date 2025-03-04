034f0f02f83e415046e92ad24220f294486a40fd
May Thazin Hlaing <99190813+maythazinh@users.noreply.github.com>
2025-03-04 18:34:17 +0900

    final shit last

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -3,7 +3,7 @@ output "website_endpoint" {
   value       = aws_s3_bucket_website_configuration.website.website_endpoint
 }
 
-output "bucket_name" {
-  description = "Name of the S3 bucket"
-  value       = aws_s3_bucket.website.id
-}
+# output "bucket_name" {
+#   description = "Name of the S3 bucket"
+#   value       = aws_s3_bucket.website.id
+# }
