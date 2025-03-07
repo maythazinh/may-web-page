774d44ce61918ec717f3028ccfbb31caa9e313f4
May Thazin Hlaing <99190813+maythazinh@users.noreply.github.com>
2025-03-07 09:14:34 +0900

    for changes the name of S3 bucket

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,14 +1,14 @@
-variable "bucket_name" {
-  description = "the name of the S3 bucke"
-  type        = string
-  default = "may-web-page"
-}
+# variable "bucket_name" {
+#   description = "the name of the S3 bucke"
+#   type        = string
+#   default = "may-web-page"
+# }
 
-variable "tags" {
-  description = "Tags for the S3 bucket"
-  type        = map(string)
-  default     = {
-    Name        = "Static Website"
-    Environment = "Production"
-  }
-}
+# variable "tags" {
+#   description = "Tags for the S3 bucket"
+#   type        = map(string)
+#   default     = {
+#     Name        = "Static Website"
+#     Environment = "Production"
+#   }
+# }
